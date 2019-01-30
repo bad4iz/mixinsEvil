81fcdbb27ce98b1e043732c3c8e37dfbc4696988
Евгений Плотников <bad4iz@gmail.com>
2019-01-30 16:57:09 +0400

    asdf

--- src/index.js
+++ src/index.js
@@ -1,6 +1,3 @@
-import _ from 'lodash';
-import './styles.css';
-
 // поменять строчки местами и посмотреть с перезагрузкой браузера
 import component from './component';
 import './header_button.css';

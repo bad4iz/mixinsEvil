4974069e4e895b0bcf4b00fefc4cd4a568aaf445
Евгений Плотников <bad4iz@gmail.com>
2019-01-30 16:52:45 +0400

    commit

--- src/component.js
+++ src/component.js
@@ -3,11 +3,15 @@ import './dashboard.css';
 export default function component() {
     const element = document.createElement('div');
     const btn = document.createElement('button');
+    const btn2 = document.createElement('button');
     btn.classList.add('button', 'dashboard_button');
+    btn2.classList.add('button', 'header_button');
 
-    btn.innerHTML = 'a am mixin a am bed!';
+    btn.innerHTML = 'a am mixin a am bed! потому что я то красный то зеленый';
+    btn2.innerHTML = 'ыыыыыыыыыыыы';
 
     element.appendChild(btn);
+    element.appendChild(btn2);
 
     return element;
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import _ from 'lodash';
 import './styles.css';
+
+// поменять строчки местами и посмотреть с перезагрузкой браузера
 import component from './component';
+import './header_button.css';
 import button from './button';
 
 document.body.appendChild(component());
